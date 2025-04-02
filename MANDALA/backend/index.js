e28730ecc8f0f61eae952cd2cd8e5c73bbf5f83f
Gibeom Choi <kcdevdes@gmail.com>
2025-04-02 16:39:46 -0400

    Create new servere structure

---
+++ MANDALA/backend/index.js
@@ -0,0 +1,55 @@
+const express = require('express');
+
+const app = express();
+const helmet = require('helmet');
+const xss = require('xss-clean');
+const mongoSanitize = require('express-mongo-sanitize');
+const compression = require('compression');
+const cors = require('cors');
+
+// set security HTTP headers
+app.use(helmet());
+
+// parse json request body
+app.use(express.json());
+
+// parse urlencoded request body
+app.use(express.urlencoded({ extended: true }));
+
+// sanitize request data
+app.use(xss());
+app.use(mongoSanitize());
+
+// gzip compression
+app.use(compression());
+
+// enable cors
+app.use(cors());
+app.options('*', cors());
+
+const server = app.use(express.json());
+
+server.listen(3000, () => {});
+
+const exitHandler = () => {
+  if (server) {
+    server.close(() => {
+      process.exit(1);
+    });
+  } else {
+    process.exit(1);
+  }
+};
+
+const unexpectedErrorHandler = () => {
+  exitHandler();
+};
+
+process.on('uncaughtException', unexpectedErrorHandler);
+process.on('unhandledRejection', unexpectedErrorHandler);
+
+process.on('SIGTERM', () => {
+  if (server) {
+    server.close();
+  }
+});
