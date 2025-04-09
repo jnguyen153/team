b2182c34fee0f03f16c3df0d18341ef7cf9c7279
amirayed7 <amirayed@macbookpro.ssw.umb.edu>
2025-04-09 16:27:58 -0400

    checkout page

--- MANDALA/frontend/src/pages/Checkout.tsx
+++ MANDALA/frontend/src/pages/Checkout.tsx
@@ -29,14 +29,15 @@ const Checkout = () => {
     console.log('Form submitted:', formData);
   };
 
-  const handleCartClick = () => {
-    // TODO: Implement cart click handler
-    console.log('Cart clicked');
-  };
+  // const handleCartClick = () => {
+  //   // TODO: Implement cart click handler
+  //   console.log('Cart clicked');
+  // };
 
   return (
     <div className="min-h-screen flex flex-col">
-      <Navbar onCartClick={handleCartClick} />
+      {/* <Navbar onCartClick={handleCartClick} /> */}
+      <Navbar />
       <main className="flex-grow pt-16 bg-gray-50">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
           <h1 className="text-3xl font-bold text-gray-900 mb-8">Checkout</h1>
