9d2fa56635c4a1b7413387df936d672b14fda034
Yanxin Lin <yanxinlin24@gmail.com>
2025-04-16 14:42:19 -0400

    Update submit_schedule inside views.py
    Modify submit_schedule to automatically create an EmployeeParameters entry
    with default values (student_id: student_id obtained from submit_schedule, min: 5, max: 20, pref: 1)

--- SCHEDULING/backend/source/scheduler/views.py
+++ SCHEDULING/backend/source/scheduler/views.py
@@ -18,18 +18,29 @@ def admin_form_submission(request):
 def submit_schedule(request):
     if request.method == 'POST':
         body = json.loads(request.body)
+        student_id = body.get('student_id', '')
+        schedule=body.get('schedule', {})
+        # Create the schedule
         StudentSchedule.objects.create(
-            student_id=body.get('student_id', ''),
-            schedule=body.get('schedule', {})
+            student_id=student_id,
+            schedule=schedule
         )
+        # check if parameters already exist for given student_id
+        if (EmployeeParameters.objects.filter(student_id=student_id).exists()) == False:
+            # create default parameters if they don't exist
+            EmployeeParameters.objects.create(
+                student_id=student_id,
+                max_hours=20,  # default max hours = 20
+                min_hours=5,   # default min hours = 5
+                preferability=1  # default preferability = 1
+            )
+        
         return JsonResponse({'status': 'schedule received'})
     return JsonResponse({'error': 'Only POST allowed'}, status=405)
 
 @csrf_exempt
 def update_parameters(request):
     if request.method == "PUT":
-        print(">>> View reached")
-        print(">>> Method:", request.method)
         body = json.loads(request.body)
         params = body.get("parameters", {})
         EmployeeParameters.objects.create(
