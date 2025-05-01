5d06a5aea240763a3bc3224f489898b316ad682c
sachijoshi <sachijoshi10@gmail.com>
2025-04-30 21:17:06 -0400

    This commit will add changes to Gallery.tsk with the artwork the clients provided

--- MANDALA/frontend/src/components/Gallery.tsx
+++ MANDALA/frontend/src/components/Gallery.tsx
@@ -1,37 +1,56 @@
 import { useState } from 'react';
+import art1 from '../images/art1.jpeg';
+import art2 from '../images/art2.jpeg';
+import art3 from '../images/art3.jpeg';
+import art4 from '../images/art4.jpeg';
+import art6 from '../images/art6.jpeg';
+
 
 // Dummy product list
 const products = [
+ 
   {
     id: 1,
-    name: 'Boho Mandala Wall Art',
-    price: 30.0,
-    image: 'https://img.freepik.com/free-vector/monochromatic-background-watercolor-with-mandalas_23-2148865944.jpg?t=st=1744495444~exp=1744499044~hmac=5312ec422b53793c77ca730e6fc1ef186b148720ea44d0e571211698029db3ec&w=900',
+    name: 'Sparkle On Your Own',
+    price: 20.0,
+    image: art1,
     category: 'Hand Drawn',
   },
   {
     id: 2,
-    name: 'Floral Mandala Poster',
-    price: 30.0,
-    image: 'https://img.freepik.com/free-vector/linear-mandala-background_23-2147886391.jpg?t=st=1744495357~exp=1744498957~hmac=923d78ca18f938911614e46c4720dc837dee2fdbb4e8e69459f94d0b998cc14b&w=900',
+    name: 'Peacock Flute Mandala',
+    price: 25.0,
+    image: art2,
     category: 'Hand Drawn',
   },
   {
     id: 3,
+    name: 'Bubu & Dudu Hug Mandala',
+    price: 18.0,
+    image: art3,
+    category: 'Hand Drawn',
+  },
+  {
+    id: 4,
+    name: 'Flower Crown Portrait',
+    price: 28.0,
+    image: art4,
+    category: 'Digital',
+  },
+  {
+    id: 5,
     name: 'Custom Name Design',
     price: 30.0,
     image: 'https://img.freepik.com/free-vector/creative-namaste-background_23-2147692899.jpg?t=st=1744495406~exp=1744499006~hmac=1e5a8bde13e01e165a4e93f55f1f1044b97c179956ec74ee8bd5cc66d398f750&w=900',
     category: 'Custom',
   },
   {
-    id: 4,
-    name: 'Digital Lotus Print',
-    price: 30,
-    image: 'https://img.freepik.com/free-vector/hand-drawn-mandala-lotus-flower-drawing_23-2149369062.jpg?t=st=1744495312~exp=1744498912~hmac=e50c894b9eff35b4d6250b475211c5b93e2435262da40008bbd84af093551d2e&w=900',
-    category: 'Digital',
+    id: 6,
+    name: 'Mandala Hearts',
+    price: 22.0,
+    image: art6,
+    category: 'Hand Drawn',
   },
-  
-  
 ];
 
 const categories = ['All', 'Hand Drawn', 'Digital', 'Custom'];
