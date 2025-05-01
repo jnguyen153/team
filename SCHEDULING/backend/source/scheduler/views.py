ba01478f0acc2c397fba7969478b36b45b92ffdc
JuliansGHUB1 <105456402+JuliansGHUB1@users.noreply.github.com>
2025-04-30 23:58:04 -0400

    merge conf. resolved

--- SCHEDULING/backend/source/scheduler/views.py
+++ SCHEDULING/backend/source/scheduler/views.py
@@ -1,11 +1,7 @@
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 import json
-<<<<<<< Updated upstream
-from .models import AdminSubmission, StudentSchedule, EmployeeParameters
-=======
 from .models import AdminSubmission, Employee, SavedSchedules
->>>>>>> Stashed changes
 
 def get_schedules(request):
     return JsonResponse({'schedules': []})
@@ -14,13 +10,8 @@ def get_schedules(request):
 def admin_form_submission(request):
     if request.method == 'POST':
         body = json.loads(request.body)
-<<<<<<< Updated upstream
-        AdminSubmission.objects.create(data=body)
-        return JsonResponse({'status': 'admin form received'}, status = 201)
-=======
         AdminSubmission.objects.create(data=body)   
         return JsonResponse({'status': 'admin form received'})
->>>>>>> Stashed changes
     return JsonResponse({'error': 'Only POST allowed'}, status=405)
 
 @csrf_exempt
@@ -28,25 +19,6 @@ def submit_schedule(request):
     if request.method == 'POST':
         body = json.loads(request.body)
         student_id = body.get('student_id', '')
-<<<<<<< Updated upstream
-        schedule=body.get('schedule', {})
-        # Create the schedule
-        StudentSchedule.objects.create(
-            student_id=student_id,
-            schedule=schedule
-        )
-        # check if parameters already exist for given student_id
-        if (EmployeeParameters.objects.filter(student_id=student_id).exists()) == False:
-            # create default parameters if they don't exist
-            EmployeeParameters.objects.create(
-                student_id=student_id,
-                max_hours=20,  # default max hours = 20
-                min_hours=5,   # default min hours = 5
-                preferability=1  # default preferability = 1
-            )
-        
-        return JsonResponse({'status': 'schedule received'}, status = 201)
-=======
         schedule_update = body.get('schedule', {})
         if (student_id == ''):
             return JsonResponse({'error': 'student_id is required'}, status=400)  
@@ -57,37 +29,12 @@ def submit_schedule(request):
             existing_employee.schedule = schedule_update
         except Employee.DoesNotExist:
             return JsonResponse({'error': 'student_id not found in database'}, status=404)
->>>>>>> Stashed changes
     return JsonResponse({'error': 'Only POST allowed'}, status=405)
 
 @csrf_exempt
 def update_parameters(request):
     if request.method == 'PUT':
         body = json.loads(request.body)
-<<<<<<< Updated upstream
-        params = body.get('parameters', {})
-        student_id=params.get('student_id', '')
-
-        if (student_id == ''):
-            return JsonResponse({'error': 'student_id is required'}, status=400)
-        
-
-        try:
-            # get the existing parameters
-            exisitng_params = EmployeeParameters.objects.get(student_id=student_id)
-            # update fields if provided in JSON 
-            if 'min' in params:
-                exisitng_params.min_hours = params.get('min')
-            if 'max' in params:
-                exisitng_params.max_hours = params.get('max')
-            if 'preferability' in params:
-                exisitng_params.preferability = params.get('preferability')
-            exisitng_params.save()
-            return JsonResponse({'status': 'Parameters updated'}, status=204)
-
-        except EmployeeParameters.DoesNotExist:
-            return JsonResponse({'error': 'student_id not found'}, status=404)
-=======
         new_params = body.get('parameters', {})
         student_id=new_params.get('student_id', '')
 
@@ -132,5 +79,4 @@ def save_schedule(request):
         existing_data = SavedSchedules.objects.first()
         new_schedule.schedules = 
         return JsonResponse({'status': 'schedule successfully saved'})
->>>>>>> Stashed changes
     return JsonResponse({'error': 'Only PUT allowed'}, status=405)
